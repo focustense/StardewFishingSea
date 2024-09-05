2789a7821d27e005ee1da6cfec4c92101ed0fa08
focustense <focustense@protonmail.com>
2024-09-05 11:30:57 -0400

    Predict bubbles on location change.

--- FishingBuddy/ModEntry.cs
+++ FishingBuddy/ModEntry.cs
@@ -1,7 +1,11 @@
-﻿namespace FishingBuddy;
+﻿using StardewModdingAPI.Events;
+
+namespace FishingBuddy;
 
 internal sealed class ModEntry : Mod
 {
+    private readonly Dictionary<string, IReadOnlyList<Splash>> splashSchedules = [];
+
     // Initialized in Entry
     private ModConfig config = null!;
 
@@ -9,5 +13,29 @@ internal sealed class ModEntry : Mod
     {
         I18n.Init(helper.Translation);
         config = helper.ReadConfig<ModConfig>();
+
+        helper.Events.GameLoop.DayStarted += GameLoop_DayStarted;
+        helper.Events.Player.Warped += Player_Warped;
+    }
+
+    private void GameLoop_DayStarted(object? sender, DayStartedEventArgs e)
+    {
+        splashSchedules.Clear();
+        AddSplashData(Game1.currentLocation);
+    }
+
+    private void Player_Warped(object? sender, WarpedEventArgs e)
+    {
+        AddSplashData(e.NewLocation);
+    }
+
+    private void AddSplashData(GameLocation location)
+    {
+        if (splashSchedules.ContainsKey(location.NameOrUniqueName))
+        {
+            return;
+        }
+        var splashes = SplashPredictor.PredictSplashes(location).ToList();
+        splashSchedules.Add(location.NameOrUniqueName, splashes);
     }
 }
